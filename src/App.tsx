094cf176666731f6297497bc0fc52a9ca12e4624
Noma9363 <noma9363@gmail.com>
2025-12-02 20:12:18 +0900

    restart with vite-react + shadcn/ui setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import {SignupForm} from "@/components/template/signup-form";
+
+function App() {
+
+  return (
+    <>
+      <SignupForm/>
+    </>
+  )
+}
+
+export default App
