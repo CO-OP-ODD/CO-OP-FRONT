94c27bbd8f54280c330368e6b1c15da87ec1766e
djdropthebeat-tech <megan0237@gmail.com>
2025-12-05 18:21:55 +0900

    수정중

---
+++ src/pages/history/history.tsx
@@ -0,0 +1,11 @@
+import NavigationPills from '../../components/history/NavigationPills';
+import TimelineItem from '';
+
+export default function HistoryPage() {
+  return (
+    <div>
+      <NavigationPills />
+      <TimelineItem />
+    </div>
+  );
+}
